2fc919e273f8053971f1e313e3fd8094a65ad5ec
unknown <you@example.com>
2022-04-25 11:12:26 +0530

    react-store

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  //  1.state
+  const [result1,setResult1] = useState([])
+  
+
+  let result = ()=>{
+   // alert('ok')
+   let x = parseInt(prompt('enter value'))
+   //alert(x)
+   //console.log(x)
+   setResult1([...result1,x])
+   
+  
+  }
+  // 3. return statement
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick ={()=>{result()}}>result</button>
+      {
+       result1.map(function(currentValue,index,arr){
+         //console.log(currentValue)
+         return (
+           <h1 key={index}>{currentValue}</h1>
+         )
+
+         
+       })
+         
+      }
     </div>
   );
 }
